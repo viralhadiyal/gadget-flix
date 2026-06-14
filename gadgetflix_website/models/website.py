83e6f330402f6e48b3edc7d034bcdf68f5e1ee33
Viral Hadiyal <123925036+viralhadiyal@users.noreply.github.com>
2026-06-14 20:07:11 +0530

    Delete gadgetflix_website directory

--- gadgetflix_website/models/website.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class Website(models.Model):
-    _inherit = "website"
-
-    gadgetflix_llms_txt_content = fields.Text(string="LLMS.txt Content")
